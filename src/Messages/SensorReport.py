3db5ecd987ae5fc63a44e015747e1ce51772bea9
Dorus van de Spijker <dorus.van.de.spijker@prodrive-technologies.com>
2020-10-25 16:12:43 +0100

    + src/Messages/SensorReport: Schema. - src/Messages/SensorReport: Removed SmartSensor report schemas.

--- src/Messages/SensorReport.py
+++ src/Messages/SensorReport.py
@@ -5,69 +5,20 @@ from micropython import const
 class SensorReport(MessageSpecification):
 
     TYPE_SENSOR_REPORT        = const(0)
-    BASE_URL_SENSOR_REPORT         = "<pn>/<id>/"
-    DATA_KEY_SENSOR_REPORT_SAMPLES  = "S"
+    SUBTYPE_SENSOR_REPORT     = const(0)
+
+    DATA_KEY_MOIST            = const(101)
+    DATA_KEY_BAT              = const(102)
+
     DIRECTION_SENSOR_REPORT   = MessageSpecification.MSG_DIRECTION_SEND
 
-    def __init__(self, subtype, url_suffix):
-        self.DataDef = {SensorReport.DATA_KEY_SENSOR_REPORT_SAMPLES: []}
+    def __init__(self):
+        self.DataDef = {SensorReport.DATA_KEY_MOIST: 0,
+                        SensorReport.DATA_KEY_BAT: 100}
+
         super().__init__(SensorReport.TYPE_SENSOR_REPORT,
-                         subtype,
+                         SensorReport.SUBTYPE_SENSOR_REPORT,
                          self.DataDef,
-                         SensorReport.BASE_URL_SENSOR_REPORT + url_suffix,
+                         "",
                          SensorReport.DIRECTION_SENSOR_REPORT)
-
-
-class SensorReportTemp(SensorReport):
-
-    NAME_TEMP = "temp"
-    SUBTYPE_TEMP = const(1)
-
-    def __init__(self):
-        super().__init__(SensorReportTemp.SUBTYPE_TEMP, SensorReportTemp.NAME_TEMP)
-
-
-class SensorReportMoist(SensorReport):
-
-    NAME_MOIST = "moist"
-    SUBTYPE_MOIST = const(2)
-
-    def __init__(self):
-        super().__init__(SensorReportMoist.SUBTYPE_MOIST, SensorReportMoist.NAME_MOIST)
-
-
-class SensorReportLight(SensorReport):
-
-    NAME_LIGHT = "light"
-    SUBTYPE_LIGHT = const(3)
-
-    def __init__(self):
-        super().__init__(SensorReportLight.SUBTYPE_LIGHT, SensorReportLight.NAME_LIGHT)
-
-
-class SensorReportMoistCalibLow(SensorReport):
-    SUBTYPE_MOIST_CALIB_LOW = const(4)
-    NAME_MOIST_CALIB_LOW = "moist_cal_l"
-
-    def __init__(self):
-        super().__init__(SensorReportMoistCalibLow.SUBTYPE_MOIST_CALIB_LOW,
-                         SensorReportMoistCalibLow.NAME_MOIST_CALIB_LOW)
-
-
-class SensorReportMoistCalibHigh(SensorReport):
-    SUBTYPE_MOIST_CALIB_HIGH = const(5)
-    NAME_MOIST_CALIB_HIGH = "moist_cal_h"
-
-    def __init__(self):
-        super().__init__(SensorReportMoistCalibHigh.SUBTYPE_MOIST_CALIB_HIGH,
-                         SensorReportMoistCalibHigh.NAME_MOIST_CALIB_HIGH)
-
-
-class SensorReportGrowth(SensorReport):
-    SUBTYPE_GROWTH = const(6)
-    NAME_GROWTH = "growth"
-
-    def __init__(self):
-        super().__init__(SensorReportGrowth.SUBTYPE_GROWTH,
-                         SensorReportGrowth.NAME_GROWTH)
 
