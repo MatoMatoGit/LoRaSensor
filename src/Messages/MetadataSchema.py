ff5cdd5dcb5234e8b6ed6b5592fb37d477dfab63
Dorus van de Spijker <dorus.van.de.spijker@prodrive-technologies.com>
2020-10-25 16:13:07 +0100

    + src/Messages/MetadataSchema: CBOR-based metadata schema.

---
+++ src/Messages/MetadataSchema.py
@@ -0,0 +1,17 @@
+from upyiot.system.Util import DeviceId
+from micropython import const
+
+MSG_SECTION_META    = const(1)
+MSG_SECTION_DATA    = const(2)
+MSG_META_VERSION    = const(10)
+MSG_META_TYPE       = const(11)
+MSG_META_ID         = const(12)
+
+Metadata = {
+    MSG_META_VERSION:   1,
+    MSG_META_TYPE:      1,
+    MSG_META_ID:        DeviceId(),
+}
+
+MetadataFuncs = {
+}
