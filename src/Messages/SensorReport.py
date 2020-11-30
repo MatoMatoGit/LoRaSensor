4ff5a2014d4a4b63deeaa92bcbb28230e38da6f6
Dorus <spijker.dorus@gmail.com>
2020-11-30 01:10:49 +0100

    + src/Messages/SensorReport: Temperature entry in message.

--- src/Messages/SensorReport.py
+++ src/Messages/SensorReport.py
@@ -9,12 +9,14 @@ class SensorReport(MessageSpecification):
 
     DATA_KEY_MOIST            = const(101)
     DATA_KEY_BAT              = const(102)
+    DATA_KEY_TEMP             = const(103)
 
     DIRECTION_SENSOR_REPORT   = MessageSpecification.MSG_DIRECTION_SEND
 
     def __init__(self):
         self.DataDef = {SensorReport.DATA_KEY_MOIST: 0,
-                        SensorReport.DATA_KEY_BAT: 100}
+                        SensorReport.DATA_KEY_BAT: 100,
+                        SensorReport.DATA_KEY_TEMP: 20}
 
         super().__init__(SensorReport.TYPE_SENSOR_REPORT,
                          SensorReport.SUBTYPE_SENSOR_REPORT,
