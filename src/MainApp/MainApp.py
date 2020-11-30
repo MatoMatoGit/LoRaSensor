6bb053bc325820475b0df4b9dda9af4a82c62218
Dorus <spijker.dorus@gmail.com>
2020-11-30 01:12:51 +0100

    + src/MainApp/MainApp: Import PowerMananger module and send sleep command in BeforeDeepSleep callback. + src/MainApp/MainApp: Run all services in one-shot mode. + src/MainApp/MainApp: Temperature sensor component, use ExtLogging module.

--- src/MainApp/MainApp.py
+++ src/MainApp/MainApp.py
@@ -5,6 +5,7 @@ from upyiot.system.SystemTime.SystemTime import SystemTime
 from upyiot.system.Service.ServiceScheduler import ServiceScheduler
 from upyiot.system.Service.ServiceScheduler import Service
 from upyiot.system.Util import ResetReason
+from upyiot.system.Util import DeviceId
 from upyiot.comm.Messaging.Message import Message
 from upyiot.comm.Messaging.MessageTemplate import MessageTemplate
 from upyiot.comm.Messaging.MessageExchange import MessageExchange
@@ -13,15 +14,18 @@ from upyiot.comm.Messaging.MessageFormatter import MessageFormatter
 from upyiot.comm.Messaging.Protocol.LoraProtocol import LoraProtocol
 from upyiot.comm.Messaging.Parser.CborParser import CborParser
 from upyiot.middleware.Sensor import Sensor
+from upyiot.drivers.Sleep.DeepSleep import DeepSleep
 from upyiot.drivers.Sensors.DummySensor import DummySensor
+from upyiot.drivers.Sensors.InternalTemp import InternalTemp
 from upyiot.drivers.Modems.Sx127x.sx127x import TTN, SX127x
 from upyiot.drivers.Modems.Sx127x.config import *
 
 # SmartSensor modules
 from Messages.LogMessage import LogMessage
 from Messages.SensorReport import SensorReport
-from Messages.MetadataSchema import *
-from Config.Hardware import Pins
+from Messages import MetadataSchema
+#from Config.Hardware import Pins
+from MainApp.PowerManager import PowerManager
 
 # micropython modules
 import network
@@ -46,20 +50,26 @@ class MainApp:
     SamplesPerMessage   = const(1)
 
     # Service intervals in seconds.
-    MsgExInterval           = const(60)
+    MsgExInterval           = const(15)
     SensorReadInterval      = const(15)
+    Lora = None
+    PowerMngr = None
 
     def __init__(self):
         return
 
     def Setup(self):
-        print("Device ID: {}".format(self.ID))
+        self.Log = ExtLogging.LoggerGet("Main")
+
+        self.Log.info("Device ID: {}".format(DeviceId.DeviceId()))
 
         rst_reason = ResetReason.ResetReason()
-        print("[Setup] Reset reason: {}".format(ResetReason.ResetReasonToString(rst_reason)))
+        self.Log.debug("Reset reason: {}".format(ResetReason.ResetReasonToString(rst_reason)))
 
         # Create driver instances.
         self.DummySensorDriver = DummySensor(self.DummySamples)
+
+        self.InternalTemp = InternalTemp()
 
         self.Ttn = TTN(ttn_config['devaddr'], ttn_config['nwkey'],
                        ttn_config['app'], country=ttn_config['country'])
@@ -70,14 +80,20 @@ class MainApp:
                 mosi = Pin(device_config['mosi'], Pin.OUT, Pin.PULL_UP),
                 miso = Pin(device_config['miso'], Pin.IN, Pin.PULL_UP))
 
-        self.Lora = SX127x(self.LoraSpi, pins=device_config, lora_parameters=lora_parameters, ttn_config=self.Ttn)
+        MainApp.Lora = SX127x(self.LoraSpi, pins=device_config, lora_parameters=lora_parameters, ttn_config=self.Ttn)
 
-        self.LoraProtocol = LoraProtocol(self.Lora)
+        self.LoraProtocol = LoraProtocol(MainApp.Lora)
 
         self.DummySensor = Sensor.Sensor(self.DIR,
                                         "Dummy",
                                         self.FILTER_DEPTH, self.DummySensorDriver,
-                                        self.FILTER_DEPTH,
+                                        samples_per_read=1,
+                                        dec_round=True,
+                                        store_data=False)
+
+        self.TempSensor = Sensor.Sensor(self.DIR,
+                                        "Temp",
+                                        self.FILTER_DEPTH, self.InternalTemp,
                                         samples_per_read=1,
                                         dec_round=True,
                                         store_data=False)
@@ -88,13 +104,20 @@ class MainApp:
 
         self.Scheduler = ServiceScheduler(self.DEEPSLEEP_THRESHOLD_SEC)
 
+        self.MsgEx.SvcModeSet(Service.MODE_RUN_ONCE)
+        self.DummySensor.SvcModeSet(Service.MODE_RUN_ONCE)
+        self.TempSensor.SvcModeSet(Service.MODE_RUN_ONCE)
+
         # Set service dependencies.
-        self.MsgEx.SvcDependencies({})
+        self.MsgEx.SvcDependencies({self.DummySensor: Service.DEP_TYPE_RUN_ALWAYS_BEFORE_RUN,
+                                    self.TempSensor: Service.DEP_TYPE_RUN_ALWAYS_BEFORE_RUN})
         self.DummySensor.SvcDependencies({})
+        self.TempSensor.SvcDependencies({})
 
         # Register all services to the scheduler.
         self.Scheduler.ServiceRegister(self.MsgEx)
         self.Scheduler.ServiceRegister(self.DummySensor)
+        self.Scheduler.ServiceRegister(self.TempSensor)
 
         self.Parser = CborParser()
         Message.SetParser(self.Parser)
@@ -106,37 +129,61 @@ class MainApp:
         self.SensorReportSpec = SensorReport()
         # self.LogMsgSpec = LogMessage()
 
+        #sensor_report_meta = {
+        #   SensorReport.MSG_META_TYPE: self.SensorReportSpec.Type
+        #}
         # Create MessageFormatters and couple them with their message specs.
         self.ReportFmt = MessageFormatter(self.MsgEx,
                                           MessageFormatter.SEND_ON_COMPLETE,
                                           self.SensorReportSpec,
-                                          self.MsgMetadata)
+                                          None)
 
         # Register message specs for exchange.
         self.MsgEx.RegisterMessageType(self.SensorReportSpec)
-        self.MsgEx.RegisterMessageType(self.LogMsgSpec)
+        # self.MsgEx.RegisterMessageType(self.LogMsgSpec)
 
         # Create observers for the sensor data.
         self.MoistObserver = self.ReportFmt.CreateObserver(SensorReport.DATA_KEY_MOIST, self.SamplesPerMessage)
         self.BatteryObserver = self.ReportFmt.CreateObserver(SensorReport.DATA_KEY_BAT, self.SamplesPerMessage)
+        self.TempObserver = self.ReportFmt.CreateObserver(SensorReport.DATA_KEY_TEMP, self.SamplesPerMessage)
 
         # Link the observers to the sensors.
         self.DummySensor.ObserverAttachNewSample(self.MoistObserver)
-
-        # Create a stream for the log messages.
-        # self.LogStream = self.LogAdapt.CreateStream(LogMessage.DATA_KEY_LOG_MSG,
-        #                                            ExtLogging.WRITES_PER_LOG)
+        self.TempSensor.ObserverAttachNewSample(self.TempObserver)
 
         # Configure the ExtLogging class.
-        # ExtLogging.ConfigGlobal(level=ExtLogging.INFO, stream=self.LogStream)
+        ExtLogging.ConfigGlobal(level=ExtLogging.INFO, stream=None, file_path="/test_log")
+
+        self.Scheduler.DeepSleep.RegisterCallbackBeforeDeepSleep(MainApp.LoraSleep)
 
         # Set intervals for all services.
         self.MsgEx.SvcIntervalSet(self.MsgExInterval)
         self.DummySensor.SvcIntervalSet(self.SensorReadInterval)
+        self.TempSensor.SvcIntervalSet(self.SensorReadInterval)
+
+        self.BatteryObserver.Update(100)
+
+        MainApp.PowerMngr = PowerManager.PowerManager()
+
+        self.Log.info("Finished initialization.")
+
 
     def Reset(self):
         self.MsgEx.Reset()
         self.DummySensor.SamplesDelete()
 
     def Run(self):
-        self.Scheduler.Run()
+        self.Log.info("Starting scheduler")
+        #self.MsgEx.SvcActivate()
+        self.Scheduler.Run(4)
+        self.Scheduler.RequestDeepSleep(20)
+
+    @staticmethod
+    def LoraSleep():
+        print("Setting LoRa modem to sleep mode.")
+        MainApp.Lora.sleep()
+        ExtLogging.Stop()
+        while True:
+            MainApp.PowerMngr.Sleep(1000 * 3590) # 86400
+            utime.sleep(10)
+
