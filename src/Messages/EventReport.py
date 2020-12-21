afe42ba85f1120efa03e685d6bc06a863d499357
Dorus <spijker.dorus@gmail.com>
2020-12-21 01:36:07 +0100

    + src/Messages/EventReport: Reports event code.

---
+++ src/Messages/EventReport.py
@@ -0,0 +1,22 @@
+from upyiot.comm.Messaging.MessageSpecification import MessageSpecification
+from micropython import const
+
+
+class EventReport(MessageSpecification):
+
+    TYPE_REPORT              = const(0)
+    SUBTYPE_EVENT_REPORT     = const(1)
+
+    DATA_KEY_EVENT            = const(104)
+
+    DIRECTION_REPORT   = MessageSpecification.MSG_DIRECTION_SEND
+
+    def __init__(self):
+        self.DataDef = {EventReport.DATA_KEY_EVENT: 0}
+
+        super().__init__(EventReport.TYPE_REPORT,
+                         EventReport.SUBTYPE_EVENT_REPORT,
+                         self.DataDef,
+                         "",
+                         EventReport.DIRECTION_REPORT)
+
--- src/Messages/SensorReport.py
+++ src/Messages/SensorReport.py
@@ -4,23 +4,23 @@ from micropython import const
 
 class SensorReport(MessageSpecification):
 
-    TYPE_SENSOR_REPORT        = const(0)
-    SUBTYPE_SENSOR_REPORT     = const(0)
+    TYPE_REPORT              = const(0)
+    SUBTYPE_SENSOR_REPORT    = const(0)
 
     DATA_KEY_MOIST            = const(101)
     DATA_KEY_BAT              = const(102)
     DATA_KEY_TEMP             = const(103)
 
-    DIRECTION_SENSOR_REPORT   = MessageSpecification.MSG_DIRECTION_SEND
+    DIRECTION_REPORT   = MessageSpecification.MSG_DIRECTION_SEND
 
     def __init__(self):
         self.DataDef = {SensorReport.DATA_KEY_MOIST: 0,
                         SensorReport.DATA_KEY_BAT: 100,
                         SensorReport.DATA_KEY_TEMP: 20}
 
-        super().__init__(SensorReport.TYPE_SENSOR_REPORT,
+        super().__init__(SensorReport.TYPE_REPORT,
                          SensorReport.SUBTYPE_SENSOR_REPORT,
                          self.DataDef,
                          "",
-                         SensorReport.DIRECTION_SENSOR_REPORT)
+                         SensorReport.DIRECTION_REPORT)
 
