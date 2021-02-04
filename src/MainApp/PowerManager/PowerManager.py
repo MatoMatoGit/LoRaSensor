1bf5844e2926e63715f6e79d8112054ecfdc4c9c
Dorus <spijker.dorus@gmail.com>
2021-02-04 22:58:17 +0100

    + src/MainApp/PowerManager/PowerManager: Implement the DeepSleepBase class.

--- src/MainApp/PowerManager/PowerManager.py
+++ src/MainApp/PowerManager/PowerManager.py
@@ -1,15 +1,31 @@
 from MainApp.PowerManager import Protocol
+from upyiot.drivers.Sleep import DeepSleepBase
+from upyiot.drivers.Sleep.DeepSleepBase import DeepSleepExceptionFailed
+from upyiot.drivers.Sleep.DeepSleepBase import DeepSleepBase
+from micropython import const
+import utime
 
 
-class PowerManager:
+class PowerManager(DeepSleepBase):
+
+    SLEEP_RETRY_MAX         = const(5)
+    SLEEP_RETRY_DELAY_SEC   = const(10)
 
     def __init__(self):
         self.Protocol = Protocol.Protocol(2400)
         return
 
-    def Sleep(self, msec):
-        self.Protocol.SendCommand(Protocol.PWR_CMD_SLEEP, int(msec / 1000))
-        return
+    def DeepSleep(self, msec):
+        self._RetrySleep(int(msec / 1000))
+
+    def DeepSleepForever(self):
+        self._RetrySleep(65000)
 
     def Status(self):
         return
+
+    def _RetrySleep(self, sec):
+        for i in range(0, self.SLEEP_RETRY_MAX):
+            self.Protocol.SendCommand(Protocol.PWR_CMD_SLEEP, sec)
+            utime.sleep(self.SLEEP_RETRY_DELAY_SEC)
+        raise DeepSleepExceptionFailed
