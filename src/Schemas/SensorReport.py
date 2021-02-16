d529034e847ba8db8b0c9fd660efe1fe9f9127d1
Dorus <spijker.dorus@gmail.com>
2021-02-16 01:30:25 +0100

    * src/Schemas/SensorReport: Message schemas for moist, temp and battery.

--- src/Schemas/SensorReport.py
+++ src/Schemas/SensorReport.py
@@ -5,22 +5,40 @@ from micropython import const
 class SensorReport(MessageSpecification):
 
     TYPE_REPORT              = const(0)
-    SUBTYPE_SENSOR_REPORT    = const(0)
 
-    DATA_KEY_MOIST            = const(101)
-    DATA_KEY_BAT              = const(102)
-    DATA_KEY_TEMP             = const(103)
+    DATA_KEY_MEASUREMENTS     = const(100)
 
     DIRECTION_REPORT   = MessageSpecification.MSG_DIRECTION_SEND
 
-    def __init__(self):
-        self.DataDef = {SensorReport.DATA_KEY_MOIST: [],
-                        SensorReport.DATA_KEY_BAT: 100,
-                        SensorReport.DATA_KEY_TEMP: 20}
+    def __init__(self, subtype):
+        self.DataDef = {SensorReport.DATA_KEY_MEASUREMENTS: []}
 
         super().__init__(SensorReport.TYPE_REPORT,
-                         SensorReport.SUBTYPE_SENSOR_REPORT,
+                         subtype,
                          self.DataDef,
                          "",
                          SensorReport.DIRECTION_REPORT)
 
+
+class MoistureSensorReport(SensorReport):
+
+    SUBTYPE_MOISTURE_REPORT = const(1)
+
+    def __init__(self):
+        super().__init__(self.SUBTYPE_MOISTURE_REPORT)
+
+
+class BatterySensorReport(SensorReport):
+
+    SUBTYPE_BATTERY_REPORT = const(2)
+
+    def __init__(self):
+        super().__init__(self.SUBTYPE_BATTERY_REPORT)
+
+
+class TemperatureSensorReport(SensorReport):
+
+    SUBTYPE_TEMPERATURE_REPORT = const(3)
+
+    def __init__(self):
+        super().__init__(self.SUBTYPE_TEMPERATURE_REPORT)
