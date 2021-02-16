fc0297e05a5ffba6856d74595099399aaae9e81b
Dorus <spijker.dorus@gmail.com>
2021-02-16 01:25:06 +0100

    + src/Config/: Config package, currently only contains Hardware.Pins.

---
+++ src/Config/Hardware/Pins.py
@@ -0,0 +1,12 @@
+from micropython import const
+
+# HW 1.0 pins
+
+CFG_HW_PIN_TEMP_EN              = const(18)
+CFG_HW_PIN_TEMP                 = const(32)
+
+CFG_HW_PIN_MOIST_EN             = const(17)
+CFG_HW_PIN_MOIST                = const(34)
+
+CFG_HW_PIN_VBAT_LVL_EN          = const(17)
+CFG_HW_PIN_VBAT_LVL             = const(34)
